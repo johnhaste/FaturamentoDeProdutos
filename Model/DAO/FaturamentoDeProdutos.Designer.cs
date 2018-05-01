8e58e22a5cdf4a51f4b047c1c8bb14d4f463fd6b
alexismayfire <calexandrelvieira@gmail.com>
2018-05-01 13:42:33 -0300

    Alteração para usar o banco de dados no Azure

--- Model/DAO/FaturamentoDeProdutos.Designer.cs
+++ Model/DAO/FaturamentoDeProdutos.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\John Haste\source\repos\FaturamentoDeProdutos\Model\DAO\FaturamentoDeProdutos.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\alexismayfire\Source\Repos\FaturamentoDeProdutos\Model\DAO\FaturamentoDeProdutos.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
